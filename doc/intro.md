c2d3685198196ddfa576d9da83712a1a134356a8
Ben Muschol <ben.muschol@airbnb.com>
2019-10-17 14:32:26 -0700

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kami2-solver
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kami2_solver/core_test.clj
@@ -0,0 +1,7 @@
+(ns kami2-solver.core-test
+  (:require [clojure.test :refer :all]
+            [kami2-solver.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
